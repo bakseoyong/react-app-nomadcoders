6c4737a9efb812cc842421b6cfac02f989c3b554
cs60181888 <qwerkdud1999@gmail.com>
2020-09-26 08:23:40 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import Movie from './Movie';
+import './App.css';
+
+class App extends Component{
+  state= {}
+
+  componentDidMount(){
+    this._getMovies();
+  }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map(movie => {
+      return <Movie 
+      title={movie.title}
+      poster={movie.medium_cover_image}
+      genres={movie.genres}
+      synopsis={movie.synopsis}
+      key={movie.id} />
+    })
+    return movies
+  }
+
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.mx/api/v2/list_movies.json?sort_by=rating')
+    .then(potato => potato.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
+  }
+
+  render(){
+    return (
+      <div className="App">
+        {this.state.movies ? this._renderMovies() : 'Loading'}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Movie.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Movie.css';
+
+function Movie({title, poster, genres, synopsis}){
+    return(
+        <div>
+            <div className="Movie__Columns">
+                <MoviePoster poster={poster}/>
+            </div>
+            <div className="Movie__Columns">
+                <h1>{title}</h1>
+                <div className="Movie__Genres">
+                    {genres.map((genre, index)=> <MovieGerne genre={genre} key={index}/>)}
+                </div>
+                <div className="Movie__Synopsis">
+                    {synopsis}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+Movie.propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.array.isRequired,
+    synopsis: PropTypes.string.isRequired
+}
+
+function MoviePoster({poster}){
+    return(
+        <img src={poster} alt="poster" className="Movie__Poster"></img>
+    )
+}
+
+MoviePoster.propTypes = {
+    poster: PropTypes.string.isRequired
+}
+
+function MovieGerne({genre}){
+    return(
+        <span className="Movie__Genre">{genre}</span>
+    )
+}
+
+MovieGerne.propTypes = {
+    genre: PropTypes.string.isRequired
+}
+
+export default Movie;
